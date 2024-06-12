3c504b402494916f50687b112574ac357bb5a4c8
PaolaBrina <paolabrina@icloud.com>
2024-06-12 13:54:35 -0300

    saidas

--- src/pages/Saidas.js
+++ src/pages/Saidas.js
@@ -1,16 +1,21 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function Saidas({navigation}) {
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+export default function Entradas({navigation}) {
     
     return (
         <View style={styles.container}>
-        <View style={styles.topBar}>
-            <Text style={styles.txtTask}> Tarefas </Text>
+          <View style={styles.topBar}>
+            <Ionicons name="chevron-back" size={30} color="white" style={styles.seta}
+              onPress={() => navigation.navigate('Home')}
+            />
+              <Text style={styles.txtSaidas}> Saidas </Text>
+            <MaterialCommunityIcons name="truck-plus-outline" size={60} color="white"  style={styles.icon}/> 
+          </View>
+
         </View>
-        
-            <Text> Saidas </Text>
-        <View style={styles.bottomBar}></View>
-    </View>
   );
 }
 
@@ -19,17 +24,27 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#ffff',
   },
+  seta:{
+    flex: 1,
+    position: 'absolute',
+    paddingLeft: 30,
+    top: 25
+  },
+  txtSaidas:{
+    color: '#fff',
+    textAlign: 'center',
+  },
+  icon:{
+    flex: 1,
+    position: 'absolute',
+    paddingLeft: 300,
+    top: 10
+  },
   topBar:{
     backgroundColor: '#093f88',
-    width: 350,
+    justifyContent: 'space-around',
+    width: 390,
     height: 80,
-    top: 50
+    top: 50,
   },
-  bottomBar:{
-    backgroundColor: '#093f88',
-    width: 370,
-    height: 80,
-    top: 650,
-    borderRadius: 20
-  }
 });
