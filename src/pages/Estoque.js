3b0b97d6357b7e9bbb275bcc527e7831c1f532bb
PaolaBrina <paolabrina@icloud.com>
2024-06-12 13:51:51 -0300

    estoque

--- src/pages/Estoque.js
+++ src/pages/Estoque.js
@@ -1,16 +1,21 @@
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Ionicons } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+
 export default function Estoque({navigation}) {
     
     return (
         <View style={styles.container}>
-        <View style={styles.topBar}>
-            <Text style={styles.txtTask}> Tarefas </Text>
+          <View style={styles.topBar}>
+            <Ionicons name="chevron-back" size={30} color="white" style={styles.seta}
+              onPress={() => navigation.navigate('Home')}
+            />
+              <Text style={styles.txtEstoque}> Estoque </Text>
+            <Feather name="box" size={60} color="white"  style={styles.icon}/> 
+          </View>
+
         </View>
-        
-            <Text> Estoque </Text>
-        <View style={styles.bottomBar}></View>
-    </View>
   );
 }
 
@@ -19,17 +24,27 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#ffff',
   },
+  seta:{
+    flex: 1,
+    position: 'absolute',
+    paddingLeft: 30,
+    top: 25
+  },
+  txtEstoque:{
+    color: '#fff',
+    textAlign: 'center',
+  },
+  icon:{
+    flex: 1,
+    position: 'absolute',
+    paddingLeft: 300,
+    top: 10
+  },
   topBar:{
     backgroundColor: '#093f88',
-    width: 350,
+    justifyContent: 'space-around',
+    width: 390,
     height: 80,
-    top: 50
+    top: 50,
   },
-  bottomBar:{
-    backgroundColor: '#093f88',
-    width: 370,
-    height: 80,
-    top: 650,
-    borderRadius: 20
-  }
 });
