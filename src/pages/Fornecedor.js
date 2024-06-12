206bd6981e14402fda4758ee8a300a0016ce0056
PaolaBrina <paolabrina@icloud.com>
2024-06-12 13:49:20 -0300

    fornecedor

--- src/pages/Fornecedor.js
+++ src/pages/Fornecedor.js
@@ -7,18 +7,15 @@ export default function Fornecedor({navigation}) {
     
     return (
         <View style={styles.container}>
-        <View style={styles.topBar}>
-          <Ionicons name="chevron-back" size={24} color="black" style={styles.seta}
-            onPress={() => navigation.navigate('Home')}
-          />
-            <Text style={styles.txtFornecedor}> Fornecedor </Text>
-            <Octicons name="people" size={50} color="white"/>
+          <View style={styles.topBar}>
+            <Ionicons name="chevron-back" size={30} color="white" style={styles.seta}
+              onPress={() => navigation.navigate('Home')}
+            />
+              <Text style={styles.txtFornecedor}> Fornecedor </Text>
+            <Octicons name="people" size={60} color="white" style={styles.icon}/> 
+          </View>
 
-            
         </View>
-        
-        <View style={styles.bottomBar}></View>
-    </View>
   );
 }
 
@@ -26,14 +23,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ffff',
-    alignItems: 'center'
   },
   seta:{
-    color: '#fff',
     flex: 1,
     position: 'absolute',
     paddingLeft: 30,
-    paddingBottom: 10
+    top: 25
+  },
+  txtFornecedor:{
+    color: '#fff',
+    textAlign: 'center',
+  },
+  icon:{
+    flex: 1,
+    position: 'absolute',
+    paddingLeft: 300,
+    top: 10
   },
   topBar:{
     backgroundColor: '#093f88',
@@ -42,15 +47,4 @@ const styles = StyleSheet.create({
     height: 80,
     top: 50,
   },
-  bottomBar:{
-    backgroundColor: '#093f88',
-    width: 370,
-    height: 80,
-    top: 650,
-    borderRadius: 20
-  },
-  txtFornecedor:{
-    color: '#fff',
-    textAlign: 'center',
-  }
 });
