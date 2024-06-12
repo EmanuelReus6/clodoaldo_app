655bd5b5d92a00fe6195894b2387b443571f8829
Paola de Almeida Brina <paolabrina@icloud.com>
2024-06-12 17:30:04 -0300

    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,20 @@
-import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import { StyleSheet, Image, Text, TextInput, View, TouchableOpacity} from 'react-native';
 
 export default function Entradas({navigation}) {
     
     return (
         <View style={styles.container}>
-          <View style={styles.topBar}>
-              <Text style={styles.txtLogin}> Login </Text>
-          </View>
+          <View style={styles.topBar}></View>
+            <View style={styles.cadastro}>
+              <TextInput
+                style={styles.input}
+                placeholder="email"
+                />
+              <TextInput
+                style={styles.input}
+                placeholder="senha"
+                />
+              </View>
             <TouchableOpacity style={styles.botao}
               onPress={() => navigation.navigate('Home')}
             >
@@ -20,14 +28,24 @@ export default function Entradas({navigation}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffff',
+    backgroundColor: '#093f88',
+    alignItems: 'center'
+  },
+  cadastro:{
+    flex: 1,
+    color: '#fff'
   },
   txtLogin:{
     color: '#fff',
     textAlign: 'center',
   },
+  input: {
+    width: '80%',
+    borderBottomWidth: 1,
+    padding: 10,
+  },
   botao: {
-    backgroundColor: '#093f88',
+    backgroundColor: '#021D43',
     justifyContent: 'center',
     width: 100,
     height: 50,
