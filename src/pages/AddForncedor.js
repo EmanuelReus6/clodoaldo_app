05d094530aa8f1a7daf186813e58615b171bce44
Paola de Almeida Brina <paolabrina@icloud.com>
2024-06-24 17:04:39 -0300

    fornecedor deleta

---
+++ src/pages/AddForncedor.js
@@ -0,0 +1,167 @@
+import { Pressable, StyleSheet, Text, View, TextInput, KeyboardAvoidingView, ScrollView  } from 'react-native';
+import React, {useState} from 'react';
+import {database, addDoc, collection} from '../../config/firebaseconfig';
+import { Ionicons } from '@expo/vector-icons';
+import { Octicons } from '@expo/vector-icons';
+
+export default function AddFornecedor({navigation}){
+
+  const [newcodigo, setNewcodigo] = useState(0)
+  const [newproduto, setNewproduto] = useState(null)
+  const [newdescricao, setNewdescricao] = useState(null)
+  const [newquantidade, setNewquantidade] = useState(0)
+  const [newquantminima, setNewquantminima] = useState(0)
+  const [newcategoria, setNewcategoria] = useState(null)
+
+  function addTask(){
+    const taskdocRef = collection(database, 'Fornecedor')
+    addDoc(taskdocRef, {
+      codigo:   newcodigo,
+      categoria: newcategoria,
+      descricao: newdescricao,
+      minimo: newquantminima,
+      nome: newproduto,
+      quantidade: newquantidade,
+      status: true,
+    })
+    navigation.navigate('Fornecedor')
+}
+    function deleteTask (id) {
+       const taskdocRef = doc(database, 'Fornecedor', id)
+        deleteDoc(taskdocRef)
+      }
+
+  return(
+    <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+    <View style={styles.container}>
+    <View style={styles.topBar}>
+    <Pressable onPress={() => navigation.navigate('Fornecedor')} style={styles.seta}>
+       <Ionicons name="chevron-back" size={30} color="white" />
+     </Pressable>
+        <Text style={styles.txtEstoque}> Fornecedor </Text>
+        <Octicons name="people" size={60} color="white" style={styles.icon}/> 
+    </View>
+    <View style={styles.containerB} >
+        <Text style={styles.inputext}> ID produto: </Text>
+        <TextInput
+          style={styles.input}
+          placeholder='Digite um numero:'
+          value={newcodigo}
+          onChangeText={setNewcodigo}
+          keyboardType="numeric"
+        />
+        <Text style={styles.inputext}> Email: </Text>
+        <TextInput
+          style={styles.input}
+          placeholder='email'
+          value={newproduto}
+          onChangeText={setNewproduto}
+        />
+        <Text style={styles.inputext}> Endereço: </Text>
+        <TextInput
+          style={styles.input}
+          placeholder='endereço'
+          value={newdescricao}
+          onChangeText={setNewdescricao}
+        />
+        <Text style={styles.inputext}> Categoria: </Text>
+        <TextInput
+          style={styles.input}
+          placeholder='Digite a categoria:'
+          value={newcategoria}
+          onChangeText={setNewcategoria}
+        />
+        <View style={styles.btnss}>
+        <Pressable style={styles.btnsave} 
+          onPress={() => {addTask()}} >
+          <Text style={styles.txtbtnsave}> Salvar </Text>
+        </Pressable>
+        
+        <Pressable onPress={() => { deleteTask(item.id) }} style={styles.btnsave}>
+        <Text style={styles.txtbtnsave}> excluir </Text>
+        </Pressable>
+        </View>
+    </View>
+    </View>
+     </ScrollView>
+     </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+   container: {
+    flex: 1,
+    backgroundColor: '#ffff',
+  },
+  seta:{
+    flex: 1,
+    position: 'absolute',
+    paddingLeft: 30,
+    top: 25
+  },
+  txtEstoque:{
+    color: '#fff',
+    textAlign: 'center',
+  },
+  icon:{
+    flex: 1,
+    position: 'absolute',
+    paddingLeft: 300,
+    top: 10
+  },
+  topBar:{
+    backgroundColor: '#093f88',
+    justifyContent: 'space-around',
+    width: 390,
+    top: 50,
+    height: 75,
+  },
+  containerB: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  input: {
+    width: '90%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    marginTop: 10,
+    borderRadius:10,
+    paddingHorizontal: 10,
+    borderColor: '#1D3557',
+    borderBottomWidth: 1,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+  },
+  btnsave: {
+    backgroundColor:  '#1D3557',
+    justifyContent:'center',
+    alignItems: 'center',
+    marginTop: 20,
+    padding: 10,
+    borderRadius: 10,
+    width: '40%',
+    marginHorizontal: 5,
+  },
+  txtbtnsave: {
+    color: '#EFF1ED',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  btnss:{
+    flexDirection: 'row',
+    justifyContent: "space-between"
+  },
+  inputext:{
+    width: '92%',
+    paddingHorizontal: 10,
+    marginTop: 10,
+    marginBottom: 5,
+    color: '#1D3557',
+  },
+})
--- src/pages/Fornecedor.js
+++ src/pages/Fornecedor.js
@@ -1,3 +1,163 @@
+import { StyleSheet, Text, View, TouchableOpacity, FlatList} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { database, doc, deleteDoc } from '../../config/firebaseconfig';
+import { onSnapshot, collection } from 'firebase/firestore';
+import Entypo from '@expo/vector-icons/Entypo';
+import { Ionicons } from '@expo/vector-icons';
+import { Octicons } from '@expo/vector-icons';
+
+export default function Fornecedor({navigation}) {
+  const [task, setTask] = useState([])
+  
+  // pega infomacoes do banco e adiciona na list
+  // seta a task com os dados do banco
+  useEffect (() => {
+    const tasksCollection = collection(database, 'Fornecedor')
+    const listen = onSnapshot(tasksCollection, (query) => {
+      const list = []
+      query.forEach((doc) => {
+        list.push({...doc.data(), id: doc.id})
+      })
+      setTask(list)
+    })
+    return ()  => listen();
+  }, [])
+
+  // funcao para deletar a task
+  function deleteTask (id) {
+    const taskdocRef = doc(database, 'Fornecedor', id)
+    deleteDoc(taskdocRef)
+  }
+
+    return (
+        <View style={styles.container}>
+          <View style={styles.topBar}>
+            <Ionicons name="chevron-back" size={30} color="white" style={styles.seta}
+              onPress={() => navigation.navigate('Home')}
+            />
+              <Text style={styles.txtFornecedor}> Fornecedor </Text>
+              <Octicons name="people" size={60} color="white" style={styles.icon}/> 
+          </View>
+          <View style={styles.containerB}>
+        <FlatList
+          data={task}
+          renderItem={({item}) => {
+            return(
+              <View>
+                <View style={styles.tasks}>
+                <Text style={styles.txtitem}> 
+                  Cod: {item.codigo}       Nome: {item.nome}</Text>
+                <Text style={styles.txtitem}>Email: {item.email}</Text>
+                <Text style={styles.txtitem}>Endereço: {item.endereco}</Text>
+                <Text style={styles.txtitem}>Telefone: {item.telefone}</Text>
+                </View>
+                
+                <View style={styles.lixo}>
+                <TouchableOpacity onPress={() => { deleteTask(item.id) }}>
+                  <Entypo name="trash" size={24} color="white"/>
+                </TouchableOpacity>
+                </View>
+                
+              </View>
+            )
+          }}
+        />
+          </View>
+
+          <TouchableOpacity style={styles.btnNewTask} onPress={() => navigation.navigate('AddFornecedor')}>
+          <Text style={styles.txtbtnNewTask}> + </Text>
+        </TouchableOpacity>
+        </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffff',
+  },
+  seta:{
+    flex: 1,
+    position: 'absolute',
+    paddingLeft: 30,
+    top: 25
+  },
+  txtFornecedor:{
+    color: '#fff',
+    textAlign: 'center',
+  },
+  icon:{
+    flex: 1,
+    position: 'absolute',
+    paddingLeft: 300,
+    top: 10
+  },
+  topBar:{
+    backgroundColor: '#093f88',
+    justifyContent: 'space-around',
+    width: 390,
+    height: 80,
+    top: 50,
+  },
+  btnNewTask:{
+    backgroundColor: '#093f88',
+    justifyContent: 'center',
+    alignItems: 'center', 
+    height: 60,
+    width: 60,
+    borderRadius: 50,
+    bottom: 30,
+    marginLeft: 20,
+    position: 'absolute'
+    
+  },
+  txtbtnNewTask:{
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#fff',
+    alignItems: 'center',
+    paddingBottom: 5
+  },
+  containerB:{
+    flex: 1,
+    top: 80,
+  },
+
+  txtitem:{
+    color: 'black',
+    fontSize:12,
+    marginLeft:20,
+    top: 8,
+
+  },
+  tasks:{
+    backgroundColor: '#cccccc',
+    marginLeft:12,
+    width: 290,
+    height: 75,
+    borderRadius: 10,
+  },
+  lixo:{
+    marginLeft: 311,
+    backgroundColor: '#093f88',
+    bottom: 74,
+    width: 40,
+    height: 40,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imgitem:{
+    height: 55,
+    width: 55,
+    position: 'absolute',
+    marginLeft: 10,
+    top: 10,
+    borderRadius:5,
+  },
+});
+
+/* 
 import { StyleSheet, Text, View } from 'react-native';
 
 import { Ionicons } from '@expo/vector-icons';
@@ -48,3 +208,4 @@ const styles = StyleSheet.create({
     top: 50,
   },
 });
+ */
